72b32f6cdb8d0f55e1789782428789e2bce4c969
Philippe SKOPAL <pskopal@nevaone.int>
2024-02-20 13:48:38 +0100

    Add unit test Calculator.add

---
+++ src/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+}
